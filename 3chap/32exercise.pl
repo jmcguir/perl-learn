99fb11060a1a1d0077a5513023a9e58eb6dcd2f6
Joel McGuire <jlmcguire4@gmail.com>
2017-12-20 14:53:49 -0600

    dir

--- 3chap/32exercise.pl
+++ 3chap/32exercise.pl
@@ -2,4 +2,11 @@
 use strict;
 use warnings;
 
+my @namelist = qw( fred betty barney dino wilma pebbles bamm-bamm );
+chomp(my @inlist = <stdin>);
+my @outlist;
+foreach (@inlist){
+    push @outlist, $namelist[$_]";
+}
+print @outlist;
 
