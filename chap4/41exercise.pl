346f4caa8481ddab7e98f7b1db0a1e1f24456431
Joel McGuire <jlmcguire4@gmail.com>
2017-12-20 13:57:35 -0600

    directory stucture

--- chap4/41exercise.pl
+++ chap4/41exercise.pl
@@ -3,5 +3,18 @@ use strict;
 use warnings;
 
 
+sub total {
+    my  $total_so_far = 0;
+    foreach(@_) {
+        $total_so_far +=  @_;
+    }
+    $total_so_far;
+}
 
-test;
+
+my @fred = qw{ 1 3 5 7 9 };
+my $fred_total = total(@fred);
+print "The total of \@fred is  $fred_total.\n";
+print "Enter some numbers on separate lines: ";
+my $user_total = total(<STDIN>);
+print "The total of those numbers is $user_total.\n";
